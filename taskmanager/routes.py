24a85662a869475008b743f89dfc1c205581a5f6
Ramil Tuazon <ramilytuazon@gmail.com>
2025-01-04 19:01:32 +0000

    update base.html

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from taskmanager import app, db
+from taskmanager.models import Genre, Book, Review
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    # Pass genres and books to the template to display on the home page
+    genres = Genre.query.all()
+    books = Book.query.all()
+    return render_template("base.html", genres=genres, books=books)
