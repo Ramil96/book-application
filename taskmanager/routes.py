545649ff8d5bad4d4130b60c9aa4c4499ac6ddf5
Ramil Tuazon <ramilytuazon@gmail.com>
2025-01-05 18:13:15 +0000

    create ability to create genre

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,30 +2,27 @@ from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Genre, Book
 
-
 @app.route("/")
 def home():
     genres = Genre.query.all()
     books = Book.query.all()
     return render_template("books.html", genres=genres, books=books)
 
-
 @app.route("/genres")
 def genres():
-    genres = Genre.query.all()
+    genres = Genre.query.order_by(Genre.genre_name).all()
     return render_template("genres.html", genres=genres)
-
 
 @app.route("/add_genre", methods=["GET", "POST"])
 def add_genre():
     if request.method == "POST":
         # Create a new genre using the form data
-        genre = Genre(genre_name=request.form.get("genre_name"))
+        genre_name = request.form.get("genre_name")
+        genre = Genre(genre_name=genre_name)
         db.session.add(genre)
         db.session.commit()
         return redirect(url_for("genres"))
     return render_template("add_genre.html")
-
 
 @app.route("/add_book", methods=["GET", "POST"])
 def add_book():
@@ -39,8 +36,6 @@ def add_book():
         return redirect(url_for("home"))
     genres = Genre.query.all()
     return render_template("add_book.html", genres=genres)
-
-
 
 @app.route("/reviews")
 def reviews():
