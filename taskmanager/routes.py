7e89c1b3870c9dbf8314b49b1c8ae532773e87b1
Ramil Tuazon <ramilytuazon@gmail.com>
2025-01-05 13:06:55 +0000

    update route functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,29 +1,27 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
-from taskmanager.models import Genre, Book, Review
+from taskmanager.models import Genre, Book
 
 
 @app.route("/")
 def home():
-    # Pass genres and books to the template to display on the home page
     genres = Genre.query.all()
     books = Book.query.all()
     return render_template("books.html", genres=genres, books=books)
 
 
-@app.route("/add_book")
-def add_book():
-    # Render the "Add Book" page
-    return render_template("add_book.html")
-
-
 @app.route("/genres")
 def genres():
-    # Render the "Genres" page
-    return render_template("genres.html")
+    genres = Genre.query.all()
+    return render_template("genres.html", genres=genres)
 
 
-@app.route("/reviews")
-def reviews():
-    # Render the "Reviews" page
-    return render_template("reviews.html")
+@app.route("/add_genre", methods=["GET", "POST"])
+def add_genre():
+    if request.method == "POST":
+        # Create a new genre using the form data
+        genre = Genre(genre_name=request.form.get("genre_name"))
+        db.session.add(genre)
+        db.session.commit()
+        return redirect(url_for("genres"))
+    return render_template("add_genre.html")
