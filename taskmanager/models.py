cf9794ae6a9b66ed3dc198a04a87e484911dbf2c
Ramil Tuazon <ramilytuazon@gmail.com>
2025-01-08 22:31:44 +0000

    add user model

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,4 +1,18 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from taskmanager import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default='user')  # 'user' or 'admin'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Genre(db.Model):
     __tablename__ = "genre"
